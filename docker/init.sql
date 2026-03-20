2004984f0d29464ea8f1ba13ed405afd9020f31c
Nguyễn Minh Quân <117068409+Wuan1604@users.noreply.github.com>
2026-03-20 11:09:33 +0700

    Rearrange the folder

--- docker/init.sql
+++ docker/init.sql
@@ -1,7 +1,1 @@
-INSERT IGNORE INTO roles (role_id, role_name) VALUES 
-(1, 'Admin'), 
-(2, 'Manager'), 
-(3, 'Editor'), 
-(4, 'Member'), 
-(5, 'Viewer'),
-(6, 'Contributor');
+// dùng để nhập dữ liệu 
