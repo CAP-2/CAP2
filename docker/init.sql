3cecc358c36dba6e2d1bb058d457c19f4c44f43d
DVQuyet <quyetmarcus04@gmail.com>
2026-03-21 10:11:17 +0700

    login,register

--- docker/init.sql
+++ docker/init.sql
@@ -1,1 +1,11 @@
-// dùng để nhập dữ liệu 
+-- 1. Đưa tất cả các đời của Họ Nguyễn (ID từ 100 đến 499) vào Cây số 1
+INSERT IGNORE INTO family_tree_members (family_tree_id, user_id)
+SELECT 1, user_id FROM users 
+WHERE user_id BETWEEN 101 AND 405;
+
+-- 2. Kiểm tra lại dữ liệu vừa chèn
+SELECT f.family_tree_name, u.first_name, u.last_name 
+FROM family_tree_members ftm
+JOIN family_trees f ON ftm.family_tree_id = f.family_tree_id
+JOIN users u ON ftm.user_id = u.user_id
+WHERE f.family_tree_id = 1;
